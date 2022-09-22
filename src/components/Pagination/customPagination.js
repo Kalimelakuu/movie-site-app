2faadcf388b1d4cab256bb4a52d7fce84b4c33f8
Kalimelakuu <kalkid.m19@gmail.com>
2022-09-22 18:06:35 -0500

    Getting done with search component

--- src/components/Pagination/customPagination.js
+++ src/components/Pagination/customPagination.js
@@ -1,23 +1,16 @@
-import { Pagination } from '@material-ui/lab';
-import {makeStyles } from "@mui/styles";
-import { createTheme ,ThemeProvider ,CssBaseline} from '@mui/material/styles'
+
+import { createTheme ,ThemeProvider } from '@mui/material/styles'
+import { Pagination } from '@mui/material'
 
 const theme = createTheme({
-    palette: {
-      mode:'dark'
-    },
-  });
-const useStyles = makeStyles(() => ({
-ul: {
-    '& .MuiPaginationItem-root': {
-        '&.Mui-selected': {
-        background: 'red',
-        color: 'white',
-        // borderRadius: '50%',
-        },
-    },
-},
-}));
+    palette:{
+        mode:"dark",
+        primary:{
+            main:"#fff"
+        }
+    }
+})
+
 
 
 const CustomPagination = ({setPage, numOfPages=10}) => {
@@ -26,27 +19,23 @@ const CustomPagination = ({setPage, numOfPages=10}) => {
         setPage(page)
         window.scroll(0,0)
     }
-    const classes = useStyles();
     return(
-        <div 
-           style={{
+        <div style={{
             width:'100%',
             display: 'flex',
             justifyContent:'center',
             marginTop:10
            }
-           }
-        >
-            <ThemeProvider  theme={theme}>
-                <Pagination 
-                
-                count={numOfPages}
-                onChange={(e) => hanldePageChange(e.target.textContent)} 
-                hideNextButton
-                hidePrevButton
-                color='primary'/>
-            </ThemeProvider >
-          
+           }>
+           <ThemeProvider  theme={theme}>
+              <Pagination
+              count={numOfPages}
+              onChange={(e) => hanldePageChange(e.target.textContent)} 
+              hideNextButton
+              hidePrevButton
+              color='primary'  
+              />
+           </ThemeProvider>
         </div>
     )
 }
--- src/pages/Search/search.js
+++ src/pages/Search/search.js
@@ -1,7 +1,125 @@
+import { Button, Tab, Tabs, TextField } from "@mui/material"
+import { createTheme ,ThemeProvider } from '@mui/material/styles'
+import { useEffect, useState } from "react"
+import SearchIcon from '@mui/icons-material/Search';
+import axios from 'axios'
+import SinglePage from "../../components/Single_Page/singlepage";
+import CustomPagination from "../../components/Pagination/customPagination";
 const Search = () => {
+
+    const [type, setType] = useState(0)
+    const [page , setPage] = useState(1)
+    const [searchText, setSearchText] = useState(" ")
+    const [content , setContent] = useState([]);
+    const [numOfPages, setNumOfPages] = useState();
+
+    const darkTheme = createTheme({
+        palette:{
+            mode:"dark",
+            primary:{
+                main:"#fff"
+            }
+        }
+    })
+
+    // useEffect(()=> async() => {
+    //     window.scroll(0,0)
+    //     const {data} = await axios.get(
+    //         `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${process.env.REACT_APP_API_KEY
+    //     }&language=en-US&query=${searchText}&page=${page}&include_adult=false`
+    //     )
+    //     setContent(data.results)
+    //     setNumOfPages(data.total_pages)
+    //     console.log(data)
+        
+    // },[type, page])
+
+    const fetchSearch = async () => {
+        try {
+          const { data } = await axios.get(
+            `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${
+              process.env.REACT_APP_API_KEY
+            }&query=${searchText}&page=${page}`
+          );
+          setContent(data.results);
+          setNumOfPages(data.total_pages);
+          // console.log(data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      useEffect(() => {
+        window.scroll(0, 0);
+        if(searchText !== " " ){
+            fetchSearch()
+        }
+        // eslint-disable-next-line
+      }, [page]);
+
+  
+    
+    
+
     return (
         <div>
-           <span  className="pageTitle">Search</span>
+        <ThemeProvider theme={darkTheme}>
+        <div className="search" style={{display:'flex',margin:'15px 0'}}>
+           <TextField
+           style={{flex:1}}
+           className="searhBox"
+           label="Search"
+           variant ="filled"
+           onChange={(e) => setSearchText(e.target.value)}
+           />
+           <Button 
+           onClick={fetchSearch}
+           variant="contained" 
+           style ={{marginLeft : 10}}>
+         
+           <SearchIcon fontSize="large"/></Button>
+           </div>
+
+           <Tabs 
+           value={type} 
+           indicatorColor='primary' 
+           textColor='primary' 
+           style={{paddingBottom : 5}}
+           onChange={(event, newValue) => {
+            setType(newValue)
+            setPage(1)
+           }} >
+              <Tab style={{width :"50%"}} label="Search Movies"/>
+              <Tab style={{width:"50%"}} label="Search TV Series"/>
+           </Tabs>
+           
+           </ThemeProvider>
+
+           <div className="trending">
+           
+           {
+            content && content.map((c) => (
+                <SinglePage
+                    key={c.id}
+                    id= {c.id}
+                    poster = {c.poster_path}
+                    title ={c.title || c.name}
+                    date= {c.first_air_date || c.release_date}
+                    media_type= {type ? "tv" : "movie"}
+                    vote_average={c.vote_average}
+                />
+    )) }
+
+            {
+                content.length < 1
+                && (type ? <h2>No Series Found</h2> : <h2>No Movie Found</h2>  )
+            }
+           </div>
+           {
+            numOfPages > 1 && (
+                <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+            )
+           }
         </div>
     )
 }
